016a2fbcf382993b8e2313696e34238a7b524f56
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-07-31 11:46:46 +0530

    first

--- LoveBaber_DSA_Series/Switch Case/src/NoteCalculator.java
+++ LoveBaber_DSA_Series/Switch Case/src/NoteCalculator.java
@@ -10,19 +10,36 @@ public class NoteCalculator {
         int nunOf20 = 0;
         int nunOf10 = 0;
         switch (price){
-            case 100:
+            default:
                 if(price >=100){
-                   nunOf100 = price /100;
-                   price/=100;
+                    nunOf100 = price /100;
+                    price %= 100;
                 }
-            case 50:
+
+        }switch (price){
+            default:
                 if(price >=50){
                     nunOf50 = price/50;
-                    price/=50;
+                    price%=50;
                 }
-            case 20:
-                break;
+
+
+        }switch (price){
+            default:
+                if(price >= 20){
+                    nunOf20 = price/20;
+                    price%=20;
+                }
+
+
+        }switch (price){
+            default:
+                if(price >=10){
+                    nunOf10 = price/10;
+                    price%=10;
+                }
 
         }
+        System.out.println(nunOf100+" , "+nunOf50+" , "+nunOf20+" , "+nunOf10);
     }
 }
